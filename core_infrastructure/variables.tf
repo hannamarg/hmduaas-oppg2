095cf1e5e6c34662a1dc69fd654f80c2dd69ef4f
hannamarg <145111528+hannamarg@users.noreply.github.com>
2024-11-06 12:59:21 +0100

    core

---
+++ core_infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+    type = string
+    description = "name of resource group"
+    default     = "rg-demo-core"
+}
+
+variable "location" {
+    type = string
+    description = "location of resource group"
+}
+
+variable "path" {
+    type = string
+    description = "path to tfstate file"
+    default     = "./terraform.tfstate"
+}
+
