0e6baf8e438e1220d256a366255e3d05a5823997
hannamarg <145111528+hannamarg@users.noreply.github.com>
2024-11-06 12:58:57 +0100

    application

---
+++ application_platform/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-demo-backend-hmhd"
+    storage_account_name = "sademobackendhmhd"
+    container_name       = "tfstate"
+    key                  = "app.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+/*
+data "terraform_remote_state" "remote_project" {
+  backend = "local"
+
+  config = {
+    path = "core_infrastructure/terraform.tfstate"
+  }
+}
+*/
+
+data "terraform_remote_state" "remote_project" {
+  backend = "azurerm" 
+
+  config = {
+    storage_account_name = "sademobackendhmhd"
+    container_name       = "tfstate"                 
+    key                  = "core.tfstate"
+    resource_group_name  = "rg-demo-backend-hmhd"
+  }
+}
+
+data "terraform_remote_state" "global" {
+  backend = "azurerm" 
+
+  config = {
+    storage_account_name = "sademobackendhmhd"
+    container_name       = "tfstate"                 
+    key                  = "global.tfstate"
+    resource_group_name  = "rg-demo-backend-hmhd"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "resource-group"
+  location = "West Europe"
+}
+
+module "load_balancer" {
+  source = "../modules/load_balancer"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  random_string       = data.terraform_remote_state.global.outputs.random_string.value
+}
+
+module "app_service" {
+  source = "../modules/app_service"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  random_string       = data.terraform_remote_state.global.outputs.random_string.value
+}
---
+++ application_platform/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+    type = string
+    description = "name of resource group"
+}
+
+variable "location" {
+    type = string
+    description = "location of resource group"
+}
+
+#hentet fra global main.tf
+output "random_string" {
+  value = data.terraform_remote_state.global.outputs.random_string
+}
