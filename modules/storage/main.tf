66433a5d31716bfc01017f199c5f2ca712729f5e
hannamarg <145111528+hannamarg@users.noreply.github.com>
2024-11-06 13:00:11 +0100

    modules

---
+++ modules/storage/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccount${var.random_string}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+# dette legges til etter apply, det funker ikke uten eksisterende storage account
+/*
+data "azurerm_storage_account" "example" {
+  name                = "storage${var.random_string}"
+  resource_group_name = var.resource_group_name
+}
+
+// vet ikke om trengs
+output "storage_account_tier" {
+  value = data.azurerm_storage_account.example.account_tier
+} 
+*/
