331ca0b19939af108ba53c275ec770e8b6bacc65
Hanna Duaas <hmduaas@stud.ntnu.no>
2024-11-11 10:31:12 +0100

    test av workflows med core

--- core_infrastructure/main.tf
+++ core_infrastructure/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-module "networking" {
+module "network" {
   source = "../modules/networking"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
