d030071c5caef9d40c827abda5ef6f84ddd11cfa
Hanna Duaas <hmduaas@stud.ntnu.no>
2024-11-12 15:24:40 +0100

    core rg workspaces

--- core_infrastructure/variables.tf
+++ core_infrastructure/variables.tf
@@ -2,7 +2,7 @@
 variable "resource_group_name" {
     type = string
     description = "name of resource group"
-    default     = "rg-hmhd-core"
+    default     = "rg-hmhd-core:${terraform.workspace}"
 }
 
 variable "location" {
