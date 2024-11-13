8b40212b55b09dd8f8ea1693106d52517dd0bd20
Hanna Duaas <hmduaas@stud.ntnu.no>
2024-11-13 11:55:31 +0100

    test av cicd global

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_security_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "network-hmhd-${var.random_string}"
+  name                = "network-hmhd${var.random_string}"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
