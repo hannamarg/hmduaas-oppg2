b7dc53dc902f187dcbb24c71c7fb24512bc4b5f0
Hanna Duaas <hmduaas@stud.ntnu.no>
2024-11-13 13:08:43 +0100

    global ny rg

---
+++ global/variables.tf
@@ -0,0 +1,12 @@
+
+variable "resource_group_name" {
+    type = string
+    description = "name of resource group"
+    default     = "rg-hmhd-global"
+}
+
+variable "location" {
+    type = string
+    description = "location of resource group"
+    default = "West Europe"
+}
